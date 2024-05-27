9efa5332effe1419ebc254d5034d980c3bdcd377
cvassalos <vassalce@gmail.com>
2024-05-27 13:35:44 -0400

    Added reverse() function to make site more dynamic

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenges = {
     "january" : "Eat no meat for the entire month!",
@@ -48,7 +49,8 @@ def monthly_challenge_by_number(request, month):
         return HttpResponseNotFound("Invalid Month")
         
     redirect_month = months[month - 1]
-    return HttpResponseRedirect("/challenges/" + redirect_month)
+    redirect_path = reverse("month_challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
 
 def monthly_challenge(request, month):
     try:
