64a7e65c24c2fa10f0941ca151b78839f95068ad
cvassalos <vassalce@gmail.com>
2024-06-02 13:13:29 -0400

    Created new index page to display all the months. Changed month links to dynamic urls and also started using tags in the HTML file

--- challenges/views.py
+++ challenges/views.py
@@ -43,16 +43,11 @@ monthly_challenges = {
 #     return HttpResponse(challenge_text)
 
 def index(request):
-    list_items = ""
     months = list(monthly_challenges.keys())
     
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month_challenge", args=[month])
-        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
-
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
 
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
