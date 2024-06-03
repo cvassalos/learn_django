8d8c7cfc0a3d2ede511686c422a2165ee15f900e
cvassalos <vassalce@gmail.com>
2024-06-03 18:34:33 -0400

    Create static folder in the challenges project folder, create challenges.css, and the block statement to head of base.html, and plugged it into index.html to get rid of ul decoration

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+
 
 monthly_challenges = {
     "january" : "Eat no meat for the entire month!",
@@ -16,31 +17,6 @@ monthly_challenges = {
     "november": "Walk for at least 20 minutes every day!",
     "december": None
 }
-
-# Create your views here.
-
-# def january(request):
-#     return HttpResponse("Eat no meat for the entire month!")
-
-# def february(request):
-#     return HttpResponse("Walk for at least 20 minutes every day!")
-
-# def march(request):
-#     return HttpResponse("Learn Djanggo for at least 20 minutes every day!")
-
-
-
-# def monthly_challenge(request, month):
-#     challenge_text = None
-#     if month == 'january':
-#         challenge_text = "Eat no meat for the entire month!"
-#     elif month == 'february':
-#         challenge_text = "Walk for at least 20 minutes every day!"
-#     elif month == 'march':
-#         challenge_text = "Learn Django for at least 20 minutes every day!"
-#     else:
-#         return HttpResponseNotFound("This month is not supported!")
-#     return HttpResponse(challenge_text)
 
 def index(request):
     months = list(monthly_challenges.keys())
@@ -67,5 +43,4 @@ def monthly_challenge(request, month):
             "month_name": month
         })
     except:
-        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
-
+        raise Http404()
