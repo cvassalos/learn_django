1a6cb5d85b9267ec43ec7a62cecbbb698f1f79b9
cvassalos <31630832+cvassalos@users.noreply.github.com>
2024-06-18 10:27:13 -0400

    Add files via upload

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -2,10 +2,10 @@ from datetime import date
 
 from django.shortcuts import render
 
-posts = [
+all_posts = [
     {
         "slug": "hike-in-the-mountaints",
-        "image": "mountain.jpg",
+        "image": "mountains.jpg",
         "author": "Maximilian",
         "date": date(2021, 7, 21),
         "title": "Mountain Hiking",
@@ -64,13 +64,30 @@ posts = [
     } 
 ]
 
+def get_date(post):
+    return post['date']
+
 # Create your views here.
 
 def starting_page(request): 
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+        "posts": latest_posts
+    })
 
 def posts(request):
-    return render(request, "blog/all-posts.html")
+    return render(request, "blog/all-posts.html", {
+        "posts": all_posts
+    })
 
 def post_detail(request, slug):
-    return render(request, "blog/post-detail.html")
+    selected_post = next(post for post in all_posts if post['slug'] == slug)
+
+    # selected_post = []
+    # for post in all_posts:
+    #     if post["slug"] == slug:
+    #         selected_post = post
+    return render(request, "blog/post-detail.html", {
+        "post": selected_post
+    })
