fed04639a606056d220cf11857b44e421fb15ec9
cvassalos <vassalce@gmail.com>
2024-06-26 17:13:30 -0400

    Added a string function for if a book is a bestseller, experimented with manually seeking out objects in the database as well as adding and deleting items

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -6,8 +6,11 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(max_length=100)
-    is_bestselling = models.BooleanField()
+    author = models.CharField(null=True, max_length=100)
+    is_bestselling = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.title} ({self.rating})"
+        return f"{self.title} ({self.rating}), Author: {self.author}, {self.bestseller_string()}"
+
+    def bestseller_string(self):
+        return "Bestseller!" if self.is_bestselling else "Not a Bestseller"
