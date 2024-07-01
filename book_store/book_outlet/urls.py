284fca74d12e98ff3dcb50c4ba8d6dc4d97cd329
cvassalos <vassalce@gmail.com>
2024-07-01 16:26:09 -0400

    Added aggregation count() and average to index page

--- book_store/book_outlet/urls.py
+++ book_store/book_outlet/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
-    path("<slug:slug>", views.book_detail, name="book_detail"), 
-    path("bestsellers", views.bestsellers, name="bestsellers")
+    path("bestsellers", views.bestsellers, name="bestsellers"),
+    path("<slug:slug>", views.book_detail, name="book_detail")
 ]
