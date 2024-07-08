641e8a279e200a36f0178dd7826e159dac7ee5d8
cvassalos <vassalce@gmail.com>
2024-07-08 18:42:56 -0400

    Created Country class in models and added a field in the Book class that is a many to many relationship to Country

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -6,6 +6,11 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+
+
 class Address(models.Model):
     street = models.CharField(max_length=80)
     postal_code = models.CharField(max_length=5)
@@ -38,6 +43,7 @@ class Book(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books")
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
+    published_countries = models.ManyToManyField(Country, null=True)
 
     def __str__(self):
         return f"{self.title} ({self.rating}), Author: {self.author}, {self.bestseller_string()}"
