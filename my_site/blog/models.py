d75e1dacd0cca54953a85494302172583d6170de
cvassalos <vassalce@gmail.com>
2024-07-10 14:49:15 -0400

    Created three models: Author, Post, Tag. Post has Author as a foreign key and Tag has a many to many relationship with Post

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email_address = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    excerpt = models.CharField(max_length=100)
+    image_name = models.CharField(max_length=30)
+    date = models.DateField()
+    slug = models.SlugField(default="", blank=True, null=False, db_index=True)
+    content = models.CharField(max_length=300)
+    author = models.ForeignKey(Author, on_delete=models.PROTECT, related_name="author")
+    
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=25)
+    post = models.ManyToManyField(Post)
