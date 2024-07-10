5a309a72246eadf25b1d1c79efd0217719a98115
cvassalos <vassalce@gmail.com>
2024-07-10 17:04:55 -0400

    Modified admin page, added better display and filters to posts page, add multiple posts and tags to database

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import Author, Post, Tag
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "date", "author",)
+    list_filter = ("author", "tags", "date",)
+    prepopulated_fields = {"slug": ("title",)}
+
 admin.site.register(Author)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Tag)
