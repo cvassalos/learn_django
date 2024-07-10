c199fc9bfa89be8fcf0469be52fab4fbbdf11a7f
cvassalos <vassalce@gmail.com>
2024-07-10 16:18:45 -0400

    Added all three models to the admin page

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -3,3 +3,6 @@ from django.contrib import admin
 # Register your models here.
 from .models import Author, Post, Tag
 
+admin.site.register(Author)
+admin.site.register(Post)
+admin.site.register(Tag)
--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,27 +1,28 @@
 from django.db import models
-from django.utils.text import slugify
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
 
 class Author(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email_address = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField()
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
     
 
-class Post(models.Model):
-    title = models.CharField(max_length=50)
-    excerpt = models.CharField(max_length=100)
-    image_name = models.CharField(max_length=30)
-    date = models.DateField()
-    slug = models.SlugField(default="", blank=True, null=False, db_index=True)
-    content = models.CharField(max_length=300)
-    author = models.ForeignKey(Author, on_delete=models.PROTECT, related_name="author")
-    
-
 class Tag(models.Model):
-    caption = models.CharField(max_length=25)
-    post = models.ManyToManyField(Post)
+    caption = models.CharField(max_length=20)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True, db_index=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, null=True, on_delete=models.SET_NULL, related_name="posts")
+    tags = models.ManyToManyField(Tag)
+    
