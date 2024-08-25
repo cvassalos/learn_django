e4f997c77fe0ac16f0f09da850e6627f9f8753bd
cvassalos <vassalce@gmail.com>
2024-08-25 10:06:51 -0400

    Added  code for errors to come up if the comment form is not filled out correctly. Comments are now filtered so the newest comes first. Added comments to the admin page

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Author, Post, Tag
+from .models import Author, Post, Tag, Comment
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ("title", "date", "author",)
     list_filter = ("author", "tags", "date",)
     prepopulated_fields = {"slug": ("title",)}
 
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "post")
+
 admin.site.register(Author)
 admin.site.register(Post, PostAdmin)
 admin.site.register(Tag)
+admin.site.register(Comment, CommentAdmin)
